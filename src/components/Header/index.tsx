628456d48722b590baadcb726441f09d4887820f
Eduardo Ramos <eduardosram10@gmail.com>
2022-08-20 00:17:29 -0300

    Ajustes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import styles from './Header.module.css'
 import Logo from '../../assets/logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import {AiFillCloseCircle} from 'react-icons/ai'
 
 export default function Header() {
   return (
@@ -14,26 +16,31 @@ export default function Header() {
               alt=" of the author"
             />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav" id={styles.contentNavItems}>
-              <li className="nav-item"  id={styles.NavItem}>
-                <Link className="nav-link active" aria-current="page" to="#">Inicio</Link>
-              </li>
-              <li className="nav-item"  id={styles.NavItem}>
-                <a className="nav-link"  href="#sectionDue">Sobre mim</a>
-              </li>
-              <li className="nav-item"  id={styles.NavItem}>
-                <a className="nav-link"  href="#sectionTrei">Projetos</a>
-              </li>
-              <li className="nav-item">
-                <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdFrZqjr51ZymNF6IaNFNsiL27RLe2cQuBpOFudb4Cz8HkbTg/viewform?usp=sf_link" className={styles.NavButton}>
+          <div className="offcanvas offcanvas-end" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-body">
+            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
+              <AiFillCloseCircle size={36} color={'#FFF'} />
+            </button>
+              <ul className="navbar-nav" id={styles.contentNavItems}>
+                <li className="nav-item" id={styles.NavItem}>
+                  <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
+                </li>
+                <li className="nav-item" id={styles.NavItem}>
+                  <a className="nav-link" href="#sectionDue">Sobre mim</a>
+                </li>
+                <li className="nav-item" id={styles.NavItem}>
+                  <a className="nav-link" href="#sectionTrei">Projetos</a>
+                </li>
+                <li className="nav-item">
+                  <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdFrZqjr51ZymNF6IaNFNsiL27RLe2cQuBpOFudb4Cz8HkbTg/viewform?usp=sf_link" className={styles.NavButton}>
                     Entre em Contato
-                </a>
-              </li>
-            </ul>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
